dbce530178cbdf3f22d4641e3ec67695ee24bd99
tigerdgp <tiger.dgp@hotmail.com>
2015-03-23 14:55:54 -0800

    Atualização da index

--- index.php
+++ index.php
@@ -1,30 +1,65 @@
 <?php
-/**
- * Example Application
- *
- * @package Example-application
- */
 
-require '../libs/Smarty.class.php';
+	/*
+	**************************************************************************
+	*	Nome: 		index.php
+	*	Descrição: 	Página inicial, responsável por gerenciar as solicitações de páginas (URL Rewrite)
+	*	Autor: 		Dérik Gomide Padua
+	*	Data: 		23/03/2015
+	*	Versão: 	1.0
+	*	Edição: 	-
+	**************************************************************************
+	*/
+	
+	//Verifica se a sessão foi iniciada, caso não tenha sido, a inicia
+	if (!isset($_SESSION)) session_start();
+	
+	//Inclusões de arquivos
+    require_once('includes/setup.php');
+    require_once('includes/funcoes.php');
+	
+	//Inicia a instância do framework smarty
+    $smarty = new Smarty_Setup();
+	
+	//Variável global com informações da página
+    global $page;
+    $page = array(
+	    'arquivo' => 'index',
+	    'title'  => 'Index',
+	    'tab'    => 0,
+	    'path'   => '[]'
+    );
 
-$smarty = new Smarty;
+    //Recebe os parametros passados na string
+    $queryx = $_SERVER['QUERY_STRING'];
+    $smarty->assign('query', $queryx);
+    //Pegar apenas os dados necessários
+    list($str, $trash) = explode('&', $queryx, 2);
+    //Pegamos a seção (ou valor)
+    list($page) = explode('=', $str, 1);
+	/*(Acresimo da verificação de idioma)
+	list($page, $idiomas) = explode('=', $str, 2);
+	*/	
 
-//$smarty->force_compile = true;
-$smarty->debugging = true;
-$smarty->caching = true;
-$smarty->cache_lifetime = 120;
-
-$smarty->assign("Name", "Fred Irving Johnathan Bradley Peppergill", true);
-$smarty->assign("FirstName", array("John", "Mary", "James", "Henry"));
-$smarty->assign("LastName", array("Doe", "Smith", "Johnson", "Case"));
-$smarty->assign("Class", array(array("A", "B", "C", "D"), array("E", "F", "G", "H"),
-                               array("I", "J", "K", "L"), array("M", "N", "O", "P")));
-
-$smarty->assign("contacts", array(array("phone" => "1", "fax" => "2", "cell" => "3"),
-                                  array("phone" => "555-4444", "fax" => "555-3333", "cell" => "760-1234")));
-
-$smarty->assign("option_values", array("NY", "NE", "KS", "IA", "OK", "TX"));
-$smarty->assign("option_output", array("New York", "Nebraska", "Kansas", "Iowa", "Oklahoma", "Texas"));
-$smarty->assign("option_selected", "NE");
-
-$smarty->display('index.tpl');
+	//Faz o include da página solicitada
+    switch($page)
+    {
+	    /*
+		case 'idioma':
+		    //Alterando o idioma do site
+		    //Verifica a veracidade dos dados em checkidioma()
+		    $_SESSION['idioma'] = $idiomas;
+		    checkidioma();
+		    header('Location: '.($_SERVER['HTTP_REFERER'] ? $_SERVER['HTTP_REFERER'] : '.'));
+		    break;
+		*/
+        //Carrega o Painel de Controle
+	    case 'painel':
+		    include 'painel.php';
+		    break;
+        //Carrega a pagina inicial
+	    default:
+		    include 'main.php';
+		    break;
+    }
+?>
