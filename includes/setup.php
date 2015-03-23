ba6e7c0ca8b51b22dc49ec40f5d7d6f61c4dda9b
tigerdgp <tiger.dgp@hotmail.com>
2015-03-23 15:56:26 -0800

    Atualização do arquivo de configuração do smarty

--- includes/setup.php
+++ includes/setup.php
@@ -13,42 +13,43 @@
 	
 	//Define a variável global de configuração
 	global $config;
-
-	// Carrega a biblioteca Smarty
-	define("SMARTY_DIR", "includes/smarty". $config['smarty'] ."/libs/");
-	require_once(SMARTY_DIR . 'Smarty.class.php');
-	require_once(SMARTY_DIR . 'SmartyPaginate.class.php');	
 	
 	//Include do arquivo de configuração
 	define('CONFIG_DIR', 'configs/');
 	require_once(CONFIG_DIR . 'config.php');
+
+	//Carrega a biblioteca Smarty
+	define('SMARTY_DIR', 'includes/smarty' . $config['smarty']['versao'] . '/libs/');
+	require_once(SMARTY_DIR . 'Smarty.class.php');
+	//require_once(SMARTY_DIR . 'SmartyPaginate.class.php');		
 	
-	//Carrega os arquivos necessarios para a aplicação
+	//Classe com função de execução do framework smarty
 	class Smarty_Setup extends Smarty {
 		function __construct()
 		{
-			// Construtor da classe.
-			// Este é chamado a cada nova instância.
+			//Construtor da classe.
+			//Este é chamado a cada nova instância.
 			parent::__construct();
 	
-			global $config;
+			//global $config;
 	
 			$this->template_dir = 'templates/';
 			$this->compile_dir  = 'templates_c/';
 			$this->config_dir   = 'configs/';
 			$this->cache_dir    = 'cache/';
 	
-			$this->debugging = $config['debug'];
+			$this->debugging 	= $config['smarty']['debug'];
 	
 			//Define os delimitadores padroes do smarty
 			//$this->left_delimiter  = '<smarty>{';
 			//$this->right_delimiter = '}</smarty>';
 	
-			$this->escape_html = TRUE;
+			$this->escape_html 	= TRUE;
 	
-			$this->caching = FALSE;// Smarty::CACHING_LIFETIME_CURRENT;
+			$this->caching = FALSE;//Smarty::CACHING_LIFETIME_CURRENT;
 			$this->assign('app_name', $config['app_name']);
 			$this->assign('logo', $config['logo']);
 		}
 	}
+	
 ?>
