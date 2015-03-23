094e23d043919596a26354b6f7364532cdb387f7
tigerdgp <tiger.dgp@hotmail.com>
2015-03-23 14:55:15 -0800

    Arquivo de funções

---
+++ includes/funcoes.php
@@ -0,0 +1,32 @@
+<?php
+
+	/*
+	**************************************************************************
+	*	Nome: 		funcoes.php
+	*	Descrição: 	Arquivo de funções do sistema
+	*	Autor: 		Dérik Gomide Padua
+	*	Data: 		23/03/2015
+	*	Versão: 	1.0
+	*	Edição: 	-
+	**************************************************************************
+	*/
+	
+	//Define a variável global de configuração
+	global $config;
+	
+	//Função para alternar idioma de exibição do sistema 
+	/*(Desativado)
+    $idiomas = array(
+	    0 => 'ptbr',
+	    1 => 'enus',
+    );
+    function checkidioma()
+    {
+	    global $idiomas;
+	    if(!isset($_SESSION['idioma']) || !isset($idiomas[$_SESSION['idioma']]))
+		    $_SESSION['idioma'] = $config['idioma'];
+    }
+    checkidioma();
+	*/
+
+?>
