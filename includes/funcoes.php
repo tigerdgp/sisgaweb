44c15923a49ca88c7d9a359bdbf338ff15375af7
tigerdgp <tiger.dgp@hotmail.com>
2015-10-24 12:02:30 -0200

    Criação de função para geração de string aleatoria

--- includes/funcoes.php
+++ includes/funcoes.php
@@ -43,5 +43,37 @@
     checkidioma();
 	*/
 	
-
+	/**
+	* Função para gerar strings aleatórias com numeros, letras e simbolos
+	*
+	* @author    Thiago Belem <contato@thiagobelem.net>
+	*
+	* @param integer $tamanho Tamanho da string a ser gerada
+	* @param boolean $maiusculas Se terá letras maiúsculas
+	* @param boolean $numeros Se terá números
+	* @param boolean $simbolos Se terá símbolos
+	*
+	* @return string A string gerada
+	*/
+	
+	function random($tamanho = 8, $maiusculas = true, $numeros = true, $simbolos = false) {
+		$lmin = 'abcdefghijklmnopqrstuvwxyz';
+		$lmai = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+		$num = '1234567890';
+		$simb = '!@#$%*-';
+		$retorno = '';
+		$caracteres = '';
+		
+		$caracteres .= $lmin;
+		if ($maiusculas) $caracteres .= $lmai;
+		if ($numeros) $caracteres .= $num;
+		if ($simbolos) $caracteres .= $simb;
+		
+		$len = strlen($caracteres);
+		for ($n = 1; $n <= $tamanho; $n++) {
+			$rand = mt_rand(1, $len);
+			$retorno .= $caracteres[$rand-1];
+		}
+		return $retorno;
+	}
 ?>
